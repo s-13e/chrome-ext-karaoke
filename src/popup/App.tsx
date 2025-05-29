fce8137a3b1ea16d05210aab02c11f62f381b2a5
13e <13e.personal@gmail.com>
2025-05-29 16:07:46 +0900

    link popup language setting, will fix contentscript on-off

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -2,8 +2,10 @@
 import './popup.css';
 import { useLangLoader } from '../i18n/useLangLoader'; // 또는 utils 폴더
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export function App() {
+  const { t } = useTranslation();
   const isLangLoaded = useLangLoader();
   const [enabled, setEnabled] = useState(false);
 
@@ -25,16 +27,15 @@ export function App() {
   return (
     <div>
       <div className="popup-header">
-        <h1>Hello, Chrome popup page!</h1>
+        <h1>{t('extName')}</h1>
         <button id="go-to-options" className="icon-button" onClick={handleOpenOptions}>
           <img src="../assets/icons/setting.png" alt="설정" width={24} height={24} />
         </button>
       </div>
       <div>
-        <label className="toggle">
-          <input type="checkbox" checked={enabled} onChange={toggleEnabled} className="toggle_checkbox" />
-          <span className="toggle_btn"></span>
-          <span>{enabled ? 'ON' : 'OFF'}</span>
+        <label className="switch">
+          <input type="checkbox" checked={enabled} onChange={toggleEnabled} />
+          <span className="slider"></span>
         </label>
       </div>
     </div>
